7c863e4fa022dc392860b1899ab1f715405df6a5
0942533 <0942533@hr.nl>
2020-04-14 20:49:27 +0200

    Create loop function

--- stasi_vs_wifi_arduino/stasi_vs_wifi_arduino.ino
+++ stasi_vs_wifi_arduino/stasi_vs_wifi_arduino.ino
@@ -32,3 +32,42 @@ void setup(){
     Serial.println("SENSOR ACTIVE");
     delay(50);
   }
+ 
+///////////////// LOOP /////////////////
+void loop(){
+
+     //If there has been detected motion
+     if(digitalRead(pirPin) == HIGH){
+       digitalWrite(ledPin, HIGH);
+       if(lockLow){ 
+         //makes sure we wait for a transition to LOW before any further output is made:
+         lockLow = false;           
+         Serial.println("---");
+         Serial.print("motion detected at ");
+         Serial.print(millis()/1000);
+         Serial.println(" sec");
+         delay(50);
+         }        
+         takeLowTime = true;
+       }
+ 
+     if(digitalRead(pirPin) == LOW){      
+       digitalWrite(ledPin, LOW);  
+ 
+       if(takeLowTime){
+        lowIn = millis();          //save the time of the transition from high to LOW
+        takeLowTime = false;       //make sure this is only done at the start of a LOW phase
+        }
+       //if the sensor is low for more than the given pause,
+       //we assume that no more motion is going to happen
+       if(!lockLow && millis() - lowIn > pause){ 
+           //makes sure this block of code is only executed again after
+           //a new motion sequence has been detected
+           lockLow = true;                       
+           Serial.print("motion ended at ");      //output
+           Serial.print((millis() - pause)/1000);
+           Serial.println(" sec");
+           delay(50);
+           }
+       }
+  }
